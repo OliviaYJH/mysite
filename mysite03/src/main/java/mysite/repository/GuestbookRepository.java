9ab6e37aca629ac9be49afc53a44da32c6fb44fe
YuJeongHyun <0703olivia@naver.com>
2025-01-08 15:04:59 +0900

    guestbook log

--- mysite03/src/main/java/mysite/repository/GuestbookRepository.java
+++ mysite03/src/main/java/mysite/repository/GuestbookRepository.java
@@ -28,4 +28,7 @@ public class GuestbookRepository {
 		return sqlSession.delete("guestbook.deleteByIdAndPassword", Map.of("id", id, "password", password));
 	}
 
+	public GuestbookVo findById(Long id) {
+		return sqlSession.selectOne("guestbook.findById", id);
+	}
 }
