d7fecb70e5137a319d6b3d38f2a1abbaeb9fad3f
YuJeongHyun <0703olivia@naver.com>
2025-01-13 10:50:24 +0900

    Exception Handler 수정

--- mysite03/src/main/java/mysite/exception/GlobalExceptionHandler.java
+++ mysite03/src/main/java/mysite/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package mysite.exception;
 
 import java.io.PrintWriter;
 import java.io.StringWriter;
+import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -9,19 +10,36 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import mysite.dto.JsonResult;
+
 @ControllerAdvice
 public class GlobalExceptionHandler {
 	private static final Log log = LogFactory.getLog(GlobalExceptionHandler.class);
 
 	@ExceptionHandler(Exception.class)
-	public String handler(Model model, Exception e) {
+	public void handler(HttpServletRequest request, HttpServletResponse response, Model model, Exception e)
+			throws Exception {
+
 		// 1. logging
 		StringWriter errors = new StringWriter();
 		e.printStackTrace(new PrintWriter(errors));
 		log.error(errors.toString());
 
-		// 2. 사과 페이지(종료)
-		model.addAttribute("errors", errors.toString());
-		return "errors/exception";
+		// 2. 요청 구분
+		// json 요청: request header의 accept: application/json (o) 반드시 있음
+		// html 요청: request header의 accept: application/json (x) 반드시 없음
+		String accept = request.getHeader("accept");
+
+		if (accept.matches(".*application/json.*")) {
+			// 3. JSON 응답
+			JsonResult jsonResult = JsonResult.fail(errors.toString());
+			
+		} else {
+			// 4. 사과 페이지(종료)
+			request.setAttribute("errors", errors.toString());
+			request.getRequestDispatcher("/WEB-INF/views/errors/exception.jsp").forward(request, response);
+		}
 	}
 }
