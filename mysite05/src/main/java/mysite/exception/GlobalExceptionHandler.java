d13d707c4928644e983e394f297a16f67297b6be
YuJeongHyun <0703olivia@naver.com>
2025-01-14 13:17:17 +0900

    mysite05

--- mysite05/src/main/java/mysite/exception/GlobalExceptionHandler.java
+++ mysite05/src/main/java/mysite/exception/GlobalExceptionHandler.java
@@ -9,6 +9,7 @@ import org.apache.commons.logging.LogFactory;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -34,9 +35,10 @@ public class GlobalExceptionHandler {
 		// html 요청: request header의 accept: application/json (x) 반드시 없음
 		String accept = request.getHeader("accept");
 
+		// 3. JSON 응답
 		if (accept.matches(".*application/json.*")) {
-			// 3. JSON 응답
-			JsonResult jsonResult = JsonResult.fail(errors.toString());
+			JsonResult jsonResult = JsonResult
+					.fail((e instanceof NoHandlerFoundException) ? "Unknown API URL" : errors.toString());
 			// 본문 내용
 			String jsonString = new ObjectMapper().writeValueAsString(jsonResult);
 
@@ -46,8 +48,13 @@ public class GlobalExceptionHandler {
 			os.write(jsonString.getBytes("utf-8"));
 			os.close();
 
+			return;
+		}
+
+		// 4. HTML 응답: 사과 페이지(종료)
+		if (e instanceof NoHandlerFoundException) {
+			request.getRequestDispatcher("/WEB-INF/views/errors/404.jsp").forward(request, response);
 		} else {
-			// 4. 사과 페이지(종료)
 			request.setAttribute("errors", errors.toString());
 			request.getRequestDispatcher("/WEB-INF/views/errors/exception.jsp").forward(request, response);
 		}
