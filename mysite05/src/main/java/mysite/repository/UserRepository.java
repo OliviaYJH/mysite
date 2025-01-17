fe1ef3ba40e3a92d46037708fe5927c7205a9da0
YuJeongHyun <0703olivia@naver.com>
2025-01-17 13:58:49 +0900

    SpringSecurity

--- mysite05/src/main/java/mysite/repository/UserRepository.java
+++ mysite05/src/main/java/mysite/repository/UserRepository.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.stereotype.Repository;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import mysite.vo.UserVo;
 
 @Repository
@@ -31,7 +33,8 @@ public class UserRepository {
 		return sqlSession.update("user.update", vo);
 	}
 
-	public UserVo findByEmail(String email) {
-		return sqlSession.selectOne("user.findByEmail", email);
+	public <R> R findByEmail(String email, Class<R> resultTypes) {
+		Map<String, Object> map = sqlSession.selectOne("user.findByEmail", email);
+		return new ObjectMapper().convertValue(map, resultTypes);
 	}
 }
--- mysite05/src/main/java/mysite/service/UserService.java
+++ mysite05/src/main/java/mysite/service/UserService.java
@@ -1,5 +1,6 @@
 package mysite.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import mysite.repository.UserRepository;
@@ -7,13 +8,17 @@ import mysite.vo.UserVo;
 
 @Service
 public class UserService {
+	private PasswordEncoder passwordEncoder;
+
 	private UserRepository userRepository;
 
-	public UserService(UserRepository userRepository) {
+	public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
 		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
 	}
 
 	public void join(UserVo userVo) {
+		userVo.setPassword(passwordEncoder.encode(userVo.getPassword()));
 		userRepository.insert(userVo);
 	}
 
@@ -30,6 +35,8 @@ public class UserService {
 	}
 
 	public UserVo getUser(String email) {
-		return userRepository.findByEmail(email);
+		UserVo userVo = userRepository.findByEmail(email, UserVo.class);
+		userVo.setPassword("");
+		return userVo;
 	}
 }
